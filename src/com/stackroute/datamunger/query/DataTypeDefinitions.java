3cffa238e400e2b1ee2f9d04afe105d50e5f8b90
rsrizwan77 <rsrizwan77@gmail.com>
2020-09-16 15:45:34 +0530

    Adding class DataTypeDefinition

--- src/com/stackroute/datamunger/query/DataTypeDefinitions.java
+++ src/com/stackroute/datamunger/query/DataTypeDefinitions.java
@@ -2,13 +2,18 @@ package com.stackroute.datamunger.query;
 
 public class DataTypeDefinitions {
 
+	private String dataTypes[];
 	/*
 	 * This class should contain a member variable which is a String array, to hold
 	 * the data type for all columns for all data types and should override
 	 * toString() method as well.
 	 */
 	public String[] getDataTypes() {
-		return null;
+
+		return dataTypes;
 	}
 
+	public void setDataTypes(String[] dataTypes) {
+		this.dataTypes = dataTypes;
+	}
 }
